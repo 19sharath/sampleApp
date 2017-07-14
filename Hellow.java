621b66faa0653f1b9ff6f7d00c841292b47d066a
19sharath <sharath1122@gmail.com>
2017-07-14 21:40:17 +0530

    chang

--- Hellow.java
+++ Hellow.java
@@ -1,11 +1,12 @@
 public class Hellow{
 
- public static void main(String[] args){
+ static public  void main(String[] args){
    for(int i=1;i<=10;i++)
    {
    System.out.println("*"+i);
    }
    for(int j=2;j<=i;j++)
    System.out.println("*");
+   
  }
 }
