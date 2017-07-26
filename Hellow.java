582a40096b9816cfc87d42c82acb987ae5666ce5
19sharath <sharath1122@gmail.com>
2017-07-26 22:21:28 +0530

    nothingmuch

--- Hellow.java
+++ Hellow.java
@@ -3,7 +3,7 @@ public class Hellow{
  static public  void main(String[] args){
    for(int i=1;i<=10;i++)
    {   
-   for(int j=2;j<=i;j++)
+   for(int j=i+1;j<=i;j++)
    System.out.print("*");
 } 
 }}
