021f4084d1cc1ebaf4efc6715ec54bc23d1a3173
19sharath <sharath1122@gmail.com>
2017-07-27 22:40:36 +0530

    che

--- Hellow.java
+++ Hellow.java
@@ -4,7 +4,7 @@ public class Hellow{
    for(int i=1;i<=10;i++)
    {   
    for(int j=1;j<=i;j++)
-   System.out.print("*");
+   System.out.println("*");
 } 
-System.out.println(" \n");
+System.out.print(" ");
 }}
