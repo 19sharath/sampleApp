12138c22bd03109f50c1479574bfa5cd75ce4ec6
19sharath <sharath1122@gmail.com>
2017-07-27 22:21:00 +0530

    checking

--- Hellow.java
+++ Hellow.java
@@ -3,7 +3,8 @@ public class Hellow{
  static public  void main(String[] args){
    for(int i=1;i<=10;i++)
    {   
-   for(int j=2;j<=i;j++)
+   for(int j=1;j<=i;j++)
    System.out.print("*");
 } 
+System.out.println(" \n");
 }}
